210e2e66d98385eda5d9ade36d249b8efa35e121
bajilogba <bajilogba@student.ccc.edu>
2024-10-23 00:52:54 -0500

    create

---
+++ testrq.py
@@ -0,0 +1,12 @@
+# DebugChapter1
+
+first = float(input("Emter the first integer: "))
+
+
+second = float(input("Enter the second integer:"))
+
+
+sum = first + second
+
+
+print ("The sum is", sum)
